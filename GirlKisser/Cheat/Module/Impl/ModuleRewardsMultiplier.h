f59fc2265c1fad075909c0a050d425bf9b727ecb
R-unic <roleyspersonal@gmail.com>
2024-04-14 16:23:45 -0400

    feat: set max rewards multiplier to 3500

--- GirlKisser/Cheat/Module/Impl/ModuleRewardsMultiplier.h
+++ GirlKisser/Cheat/Module/Impl/ModuleRewardsMultiplier.h
@@ -3,7 +3,7 @@
 #include "../../Gui/imgui_hooker.h"
 
 class ModuleBase;
-static GKSliderInt __rewards_multiplier_amount = GKSliderInt("Amount", 3, 1, 50000);
+static GKSliderInt __rewards_multiplier_amount = GKSliderInt("Amount", 3, 1, 3500);
 static GKModule __rewards_multiplier = { "Rewards Multiplier", REWARDS, 0x0, true, {(GKSetting<>*) &__rewards_multiplier_amount} };
 
 class ModuleRewardsMultiplier : ModuleBase
